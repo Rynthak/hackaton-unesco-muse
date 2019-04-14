44060b8a1714c57c9e18ce5c41d6cd31f0886485
Rynthak <mehdi.boutoubat@gmail.com>
2019-04-14 13:20:17 +0200

    Add muse

---
+++ muse/src/components/AddMuse.vue
@@ -0,0 +1,10 @@
+<template>
+  <b-container>
+    <h1 class="title">Add new Muse</h1>
+  </b-container>
+</template>
+<script>
+export default {
+  name: "AddMuse"
+};
+</script>
--- muse/src/router/index.js
+++ muse/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-//import AddAds from '@/components/AddMuse'
+import AddMuse from '@/components/AddMuse'
 
 Vue.use(Router);
 
@@ -10,5 +10,10 @@ export default new Router({
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/addmuse',
+    name: 'AddMuse',
+    component: AddMuse
   }]
 })
