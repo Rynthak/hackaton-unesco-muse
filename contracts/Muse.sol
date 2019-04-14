83725a58f994c23fa920b2876aa012abc989304e
Rynthak <mehdi.boutoubat@gmail.com>
2019-04-14 12:07:10 +0200

    Add minor fix

--- contracts/Muse.sol
+++ contracts/Muse.sol
@@ -1,7 +1,29 @@
 pragma solidity ^0.5.2;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+
 contract Muse is Ownable{
 	
+   using SafeMath for uint256;
+   struct Muse {
+   		string name;
+   		string data;
+   }
 	
+   Muse[] public muses;
+   uint public nbMuse ;
+   
+   function addMuse(string memory _name,string memory _data) public{
+   	    Muse memory _muse = Muse({
+			name: _name,
+			data : _data
+        });  
+        
+        muses.push(_muse) ;  
+        nbMuse= nbMuse.add(1);
+		 
+	}
 	
 }
